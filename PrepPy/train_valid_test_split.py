1b60dcc52843a2e78f4fc6117a6b4fbea3411299
George <george,.thio@gmail.com>
2020-03-07 17:46:06 -0800

    Updated function and test

--- PrepPy/train_valid_test_split.py
+++ PrepPy/train_valid_test_split.py
@@ -1,10 +1,12 @@
 import numpy as np
-
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def train_valid_test_split(X, y, test_size=None, valid_size=0, train_size=None, stratify=None, random_state=None, shuffle=True):
+def train_valid_test_split(X, y, valid_size=0.25, test_size=None, stratify=None, random_state=None, shuffle=True):
     """
-    Split arrays or matrices into random train, validation and test subsets
+    Splits arrays or matrices into random train, validation and test subsets
+    The proprotion of the train set relative to the input data will be valid_size * (1 - test)
+    ....
     
     Parameters
     ----------
@@ -12,20 +14,14 @@ def train_valid_test_split(X, y, test_size=None, valid_size=0, train_size=None, 
     Allowable inputs are lists, numpy arrays, scipy-sparse matrices or pandas dataframes
     
     test_size: float or None, optional (default=None)
-        If float, a value between 0.0 and 1.0 to represent the proportion of the input dataset to
+        float, a value between 0.0 and 1.0 to represent the proportion of the input dataset to
           comprise the size of the test subset
-        If None, the value is set to the complement of the sum of the train_size and valid_size
-        If train_size and valid_size are also None, it will be set to 0.25
-      
-    valid_size: float or None, (default=None)
-        If float, a value between 0.0 and 1.0 to represent the proportion of the input dataset to
+        If None, the value is set to 0.25
+        
+    valid_size: float or None, (default=0.25)
+        float, a value between 0.0 and 1.0 to represent the proportion of the input dataset to
           comprise the size of the validation subset
-        If None, the value will be set to 0.0
-      
-    train_size: float or None, (default=None)
-        If float, a value between 0.0 and 1.0 to represent the proportion of the input dataset to
-          comprise the size of the train subset
-        If None, the value will be set to the complement of the test_size and valid_size
+        Default value is set to 0.25  
   
     stratify: array-like or None (default=None)
         If not None, splits categorical data in a stratified fashion preserving the same proportion of
@@ -60,7 +56,7 @@ def train_valid_test_split(X, y, test_size=None, valid_size=0, train_size=None, 
     >>> list(y)
     [0, 1, 2, 3, 4, 5, 6, 7]
     
-    >>> X_train, X_valid, y_train, y_valid, X_test, y_test = pp.train_valid_test_split(X, y, test_size=0.25, valid_size=0.25, random_state=777)
+    >>> X_train, X_valid, X_test, y_train, y_valid, y_test = pp.train_valid_test_split(X, y, test_size=0.25, valid_size=0.25, random_state=777)
         
     >>> X_train
     array([[4, 5],
@@ -112,26 +108,17 @@ def train_valid_test_split(X, y, test_size=None, valid_size=0, train_size=None, 
     # Split into `test` set and `resplit` set to be resplit into `train` and `valid` sets 
     X_resplit, X_test, y_resplit, y_test = train_test_split(X, 
                                                             y, 
-                                                            test_size=None, 
-                                                            train_size=None,
-                                                            stratify=None,
-                                                            random_state=1,
-                                                            shuffle=True)    
+                                                            test_size=test_size, 
+                                                            stratify=stratify,
+                                                            random_state=random_state,
+                                                            shuffle=shuffle)    
+
     
-    # Initialize splitting ratios for `train` and `valid` sets
-    train_ratio = 0
-    valid_ratio = 0
-    
-    # Calculate ratios for second call to `train_test_split` to produce `valid` and `train` sets 
-    valid_ratio = valid_size / (1 - test_size)
-    train_ratio = 1 - valid_ratio
-        
     X_train, X_valid, y_train, y_valid  = train_test_split(X_resplit,
                                                            y_resplit, 
-                                                           test_size=valid_ratio, 
-                                                           train_size=train_ratio,
-                                                           stratify=None,
-                                                           random_state=1,
-                                                           shuffle=True)
+                                                           test_size=valid_size, 
+                                                           stratify=stratify,
+                                                           random_state=random_state,
+                                                           shuffle=shuffle)
                                                            
     return X_train, X_valid, X_test, y_train, y_valid, y_test
