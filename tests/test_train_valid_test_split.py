00a1f5c0f8754132860bd2d2bc5a408b27ab33a0
George <george,.thio@gmail.com>
2020-03-13 11:18:08 -0700

    Deleted duplicate test and renamed to match function

--- tests/test_train_valid_test_split.py
+++ tests/test_train_valid_test_split.py
@@ -1,9 +1,8 @@
-from PrepPy import PrepPy as pp 
+
+from PrepPy import train_valid_test_split 
 
 import pandas as pd
 import numpy as np
-
-X, y = np.arange(16).reshape((8, 2)), list(range(8))
 
 # Check data input types and parameters
 
@@ -12,17 +11,15 @@ def test_train_test_valid_split():
     This script will test the output of the train_valid_test_split function
     which splits arrays or matrices into random train, validation and test subsets
  
-    The proportion of the train set relative to the input data will be valid_size * (1 - test_size)
+    The proportion of the train set relative to the input data will be valid_size * (1 - test_size).
     """
-    
-    output = pp.train_valid_test_split(X,y)
-    
-    assert(output['X_train'].shape[0] == 4)
-    assert(output['X_valid'].shape[0] == 2)
-    assert(output['X_test'].shape[0] == 2)
-    assert(output['y_train'].shape[0] == 4)
-    assert(output['y_valid'].shape[0] == 2)
-    assert(output['y_test'].shape[0] == 2)
-    
+
+    X, y = np.arange(16).reshape((8, 2)), list(range(8))
+
+    X_train, X_valid, X_test, y_train, y_valid, y_test = train_valid_test_split.train_valid_test_split(X, y)
+ 
+    assert(len(X_train) == 4)
+    assert(len(X_valid) == 2)
+    assert(len(X_test) == 2)
     
     
