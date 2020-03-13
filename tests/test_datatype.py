b8a5cba915749f715972b873ba6e3873b1c8ec47
George <george,.thio@gmail.com>
2020-03-13 16:29:26 -0700

    datatype, onehot, ttvs now PEP8 compliant

--- tests/test_datatype.py
+++ tests/test_datatype.py
@@ -1,8 +1,6 @@
 # from PrepPy import PrepPy as pp
 from PrepPy import datatype
-import pytest
 import pandas as pd
-import numpy as np
 
 test_dict = {'cat1': ['apple', None, 'pear', 'banana', 'blueberry', 'lemon'],
              'num1': [0, 1, 2, 3, 4, 5],
@@ -12,8 +10,13 @@ test_dict = {'cat1': ['apple', None, 'pear', 'banana', 'blueberry', 'lemon'],
 
 test_data = pd.DataFrame(test_dict)
 
+
 def test_datatype1():
-    assert datatype.data_type(test_data)[0].equals(test_data[['num1', 'num2', 'num3']])
-    
+    assert datatype.data_type(test_data)[0].equals(test_data[['num1',
+                                                              'num2',
+                                                              'num3']])
+
+
 def test_datatype2():
-    assert datatype.data_type(test_data)[1].equals(test_data[['cat1', 'cat2']])
+    assert datatype.data_type(test_data)[1].equals(test_data[['cat1',
+                                                              'cat2']])
