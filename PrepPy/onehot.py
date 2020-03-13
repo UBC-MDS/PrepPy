e10e8ff47c2953954aa9b6bccda9310ba4260f0d
Matt <matthew.at.connell@gmail.com>
2020-03-12 18:33:36 -0700

    onehot flake compliance

--- PrepPy/onehot.py
+++ PrepPy/onehot.py
@@ -1,23 +1,26 @@
 from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+import pandas as pd
 
-def one_hot(cols, train, valid = None, test = None):
+
+def one_hot(cols, train, valid=None, test=None):
     """
     One-hot encodes features of categorical type
-    
+
     Arguments:
     ---------
     cols : list
         list of column names
-        
+
     train : pandas.DataFrame
         The train set from which the columns come
-        
+
     valid : pandas.DataFrame
         The validation set from which the columns come
-        
+
     test : pandas.DataFrame
         The test set from which the columns come
-        
+
     Returns
         train_encoded, valid_encoded, test_encoded : pandas.DataFrames
             The encoded DataFrames
@@ -34,21 +37,24 @@ def one_hot(cols, train, valid = None, test = None):
             0               0           1
     """
     ohe = OneHotEncoder(sparse=False)
-    
+
     names = []
-    
+
     for i in train[cols].columns:
         for j in np.sort(train[i].unique()):
             names.append(i + '_' + str(j))
 
     train_encoded, valid_encoded, test_encoded = (None, None, None)
-            
-    train_encoded = pd.DataFrame(ohe.fit_transform(train[cols]), columns = names)
+
+    train_encoded = pd.DataFrame(ohe.fit_transform(train[cols]),
+                                columns=names)
 
     if valid is not None:
-        valid_encoded = pd.DataFrame(ohe.transform(valid[cols]), columns = names)
-    
+        valid_encoded = pd.DataFrame(ohe.transform(valid[cols]),
+                                        columns=names)
+
     if test is not None:
-        test_encoded = pd.DataFrame(ohe.transform(test[cols]), columns = names)
-    
+        test_encoded = pd.DataFrame(ohe.transform(test[cols]),
+                                    columns=names)
+
     return train_encoded, valid_encoded, test_encoded
