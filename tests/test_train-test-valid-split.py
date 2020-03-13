2ca3f303c6767421fa65c7d148977094be021a39
Matt <matthew.at.connell@gmail.com>
2020-03-12 20:01:24 -0700

    train_test_split passing

--- tests/test_train-test-valid-split.py
+++ tests/test_train-test-valid-split.py
@@ -1,12 +1,12 @@
 
-from PrepPy.test_train_test_valid_split import test_train_test_valid_split 
+from PrepPy import train_valid_test_split 
 
 import pandas as pd
 import numpy as np
 
 # Check data input types and parameters
 
-def test_test_train_test_valid_split():
+def test_train_test_valid_split():
     """
     This script will test the output of the train_valid_test_split function
     which splits arrays or matrices into random train, validation and test subsets
@@ -16,7 +16,7 @@ def test_test_train_test_valid_split():
 
     X, y = np.arange(16).reshape((8, 2)), list(range(8))
 
-    X_train, X_valid, X_test, y_train, y_valid, y_test = test_train_test_valid_split(X, y)
+    X_train, X_valid, X_test, y_train, y_valid, y_test = train_valid_test_split.train_valid_test_split(X, y)
  
     assert(len(X_train) == 4)
     assert(len(X_valid) == 2)
