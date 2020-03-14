5a17263f26220539074d69329d2560cd95f2942a
Matt <matthew.at.connell@gmail.com>
2020-03-13 19:25:36 -0700

    passing flake8

--- docs/conf.py
+++ docs/conf.py
@@ -20,9 +20,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
-
 import PrepPy
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ---------------------------------------------
 
--- tests/test_onehot.py
+++ tests/test_onehot.py
@@ -8,22 +8,22 @@ helperdata1 = pd.DataFrame(np.array([['monkey'],
                            columns=['animals'])
 
 
-def one_hot_test1():
-    output = one_hot(cols=['animals'],
-                     train=helperdata1)
+def onehot_test1():
+    output = onehot.onehot(cols=['animals'],
+                           train=helperdata1)
     assert(len(output.columns) == helperdata1.shape[0])
 
 
-def one_hot_test2():
-    output = one_hot(cols=['animals'],
-                     train=helperdata1,
-                     valid=helperdata1)
+def onehot_test2():
+    output = onehot.onehot(cols=['animals'],
+                           train=helperdata1,
+                           valid=helperdata1)
     assert(len(output) == 2)
 
 
-def one_hot_test3():
-    output = one_hot(cols=['animals'],
-                     train=helperdata1,
-                     valid=helperdata1,
-                     test=helperdata1)
+def onehot_test3():
+    output = onehot.onehot(cols=['animals'],
+                           train=helperdata1,
+                           valid=helperdata1,
+                           test=helperdata1)
     assert(len(output) == 3)
